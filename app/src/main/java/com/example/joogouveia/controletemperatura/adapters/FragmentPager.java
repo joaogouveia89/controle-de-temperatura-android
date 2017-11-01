6891d8edb4964c6635021dff19eb799fe7c88c45
unknown <j.lucas.gouveia@gmail.com>
2017-11-01 01:48:28 -0200

    it last set the label with the month

---
+++ app/src/main/java/com/example/joogouveia/controletemperatura/adapters/FragmentPager.java
@@ -0,0 +1,8 @@
+package com.example.joogouveia.controletemperatura.adapters;
+
+/**
+ * Created by João Gouveia on 31/10/2017.
+ */
+
+public class FragmentPager {
+}
---
+++ app/src/main/java/com/example/joogouveia/controletemperatura/custom/calendar/CustomCalendar.java
@@ -0,0 +1,8 @@
+package com.example.joogouveia.controletemperatura.custom.calendar;
+
+/**
+ * Created by João Gouveia on 01/11/2017.
+ */
+
+public class CustomCalendar {
+}
